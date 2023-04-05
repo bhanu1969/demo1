913430c70e10a11f8c2b13012ec5ded50ab7a7dd
bhanu1969 <129929080+bhanu1969@users.noreply.github.com>
2023-04-05 11:19:47 +0530

    Add files via upload

---
+++ loops.py
@@ -0,0 +1,51 @@
+#iterator loops
+#examples of loops and while loops
+
+'''for i in range(10):
+    print(i)
+    
+for i in range(0,15,2):
+    print(i)
+    
+a=["bangalore","delhi","mumbai"]
+for x in a:
+    print(a)
+
+for i in range(15):
+    if i==5:
+        break
+    print(i)
+
+for i in range(25):
+    if i==10:
+        continue
+    print(i)
+    
+for i in range(5):
+    pass
+    
+i=1
+while i<5:
+    print(i)
+    i+=1
+    
+i=10
+while i<15:
+    print(i)
+    i=i+1 '''
+
+
+print("enter your details:")
+loginname=(input("enter your name:"))
+age=int(input("your age"))
+print("thanks")
+
+    
+    
+
+
+    
+
+
+
+
